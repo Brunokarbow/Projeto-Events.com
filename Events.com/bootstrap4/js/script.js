d490f086130fb5ac80453c18b4f3289409be6253
Bruno Januario <96805693+Brunokarbow@users.noreply.github.com>
2023-09-08 11:33:29 -0300

    atualização.1
    atualização.1

---
+++ Events.com/bootstrap4/js/script.js
@@ -0,0 +1,10 @@
+/* PARTE DO MENU*/
+
+function openNav(){
+    document.getElementById('myNav').style.width = '20%'
+  
+}
+
+function closeNav(){
+    document.getElementById('myNav').style.width = '0%'
+}
